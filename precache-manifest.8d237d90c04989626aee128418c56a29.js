65bc68ffcd8b1eda7103aff4b12b9e4b463f40b3
benacq <benacq44@gmail.com>
2020-10-25 02:12:04 +0000

    Updates

---
+++ precache-manifest.8d237d90c04989626aee128418c56a29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21094568d6f16f87c69eabe437b08676",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "98eb31eb761aeb7cad80",
+    "url": "/portfolio/static/css/main.6eee80dc.chunk.css"
+  },
+  {
+    "revision": "d4cdba6319b693e8e065",
+    "url": "/portfolio/static/js/2.25f13879.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.25f13879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98eb31eb761aeb7cad80",
+    "url": "/portfolio/static/js/main.e98ef33e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "70cb152365cdd5f27f75c8679eed4f73",
+    "url": "/portfolio/static/media/benacq-2.70cb1523.jpg"
+  },
+  {
+    "revision": "031cd4bd2e25ccf2b3eedc187ef44f3d",
+    "url": "/portfolio/static/media/me2.031cd4bd.jpg"
+  }
+]);
